7e086b502116cebdf0f5fbcbc95d0eee6f99e9ef
Andreas Norris <norrs.andrew00@gmail.com>
2026-03-06 16:01:16 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
-  {
-    name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-  },
-  {
-    name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-  },
-  {
-    name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-  },
-  {
-    name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-  },
-  {
-    name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
-];
-
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
@@ -36,6 +9,9 @@ const editProfileDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input",
 );
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
@@ -43,32 +19,9 @@ const newPostFormEl = newPostModal.querySelector(".modal__form");
 const newPostImageInput = newPostModal.querySelector("#card-image-input");
 const newPostCaptionInput = newPostModal.querySelector("#post-caption-input");
 
-const profileNameEl = document.querySelector(".profile__name");
-const profileDescriptionEl = document.querySelector(".profile__description");
-const cardsList = document.querySelector(".cards__list");
-
-function createCard(title, imageUrl) {
-  const cardElement = document.createElement("li");
-  cardElement.classList.add("card");
-
-  cardElement.innerHTML = `
-    <img src="${imageUrl}" alt="${title}" class="card__image" />
-    <div class="card__content">
-      <h2 class="card__title">${title}</h2>
-      <button type="button" class="card__like-btn"></button>
-    </div>
-  `;
-
-  return cardElement;
-}
-
-initialCards.forEach(function (card) {
-  const cardElement = createCard(card.name, card.link);
-  cardsList.append(cardElement);
-});
-
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
@@ -97,12 +50,8 @@ function handleProfileFormsSubmit(evt) {
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
 
-  const imageUrl = newPostImageInput.value;
-  const caption = newPostCaptionInput.value;
-
-  const cardElement = createCard(caption, imageUrl);
-
-  cardsList.prepend(cardElement);
+  console.log("Image URL:", newPostImageInput.value);
+  console.log("Caption:", newPostCaptionInput.value);
 
   newPostFormEl.reset();
   newPostModal.classList.remove("modal_is-opened");
